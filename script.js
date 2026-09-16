2250c90341bcd05f4a207582019fb0a99fa5e204
Aryan Hapaliya <hapaliyaaryan24@gmail.com>
2026-09-17 00:02:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function calculateAge(){
         m3 = 11;
         y3--;
     }
-    result.innerHTML = `You are <span>${y3}</span> years, <span>${m3}</span> and <span>${d3}</span> days old`;
+    result.innerHTML = `You are <span>${y3}</span> years, <span>${m3}</span> Months and <span>${d3}</span> days old`;
 }
 
 function getDaysInMonth(year, month){
